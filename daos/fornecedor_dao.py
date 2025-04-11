d7822893d85a16ba32edc034982f66e6cd14f439
ROMULO BARRETO DA SILVA <romubah@gmail.com>
2025-04-11 13:35:17 -0300

    feat: adiciona a dao fornecedor e a funcao de carregar e salvar a classe

---
+++ daos/fornecedor_dao.py
@@ -0,0 +1,22 @@
+import json
+import os
+
+class FornecedorDao:
+    caminho_arquivo = os.path.join("database", "fornecedor.json")
+    
+    @classmethod
+    def carregar_fornecedor(cls):
+        try:
+            with open(cls.caminho_arquivo, "r") as arq:
+                return json.load(arq)
+        except (FileNotFoundError, json.JSONDecodeError):
+            return []
+        
+    @classmethod    
+    def salvar_fornecedor(cls, categorias):
+        try:
+            with open(cls.caminho_arquivo, "w") as arq:
+                json.dump(categorias, arq, indent=4)
+                return True, "✅ Dados salvos com sucesso."
+        except Exception as e:
+            return False, "❌ Dados não salvos por erro, favor, validar."
