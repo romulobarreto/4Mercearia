a99c1b0f58264bad188495e510aeb76b708db821
Romulo Barreto da Silva <romubah@gmail.com>
2025-04-15 22:43:12 -0300

    feat: adiciona a função de detalhar e de excluir as categorias

--- controllers/categoria_controller.py
+++ controllers/categoria_controller.py
@@ -14,7 +14,7 @@ class CategoriaController:
         
         for categoria in categorias:
             if categoria["nome"] == nome:
-                return False, "⚠️ A categoria já está cadastrada."
+                return False, f"\n⚠️ A categoria {nome} já está cadastrada."
         
         # Define o valor do maior_id
         id = max([categoria["id"] for categoria in categorias], default=0) + 1
@@ -32,6 +32,58 @@ class CategoriaController:
             return True, mensagem
         else:
             return False, mensagem
+        
 
 
+    @staticmethod
+    def detalhar_categorias():
+        # Carregar categorias
+        categorias = CategoriaDao.carregar_categoria()
 
+        # Verifica se existe categoria cadastrada
+        if not categorias:
+            return False, "\n⚠️ A lista de categorias está vazia!"
+        
+        # Detalhar categorias
+        lista_formatada = "\n📋 Lista de usuários cadastrados:\n"
+        for index, categoria in enumerate(sorted(categorias, key=lambda c: c["nome"]), start=1):
+            lista_formatada += f"{index}°: {categoria["nome"].upper()}\n"
+        lista_formatada += "---------------------------"
+
+        return True, lista_formatada
+    
+
+    
+    @staticmethod
+    def excluir_categoria(nome):
+        # Carrega categorias
+        categorias = CategoriaDao.carregar_categoria()
+
+        # Valida se existe categoria cadastrada
+        if not categorias:
+            return False, "⚠️ Não existe nenhuma categoria para excluir."
+
+        # Valida o nome da categoria
+        if not nome:
+            return False, "⚠️ A categoria não pode estar vazia."
+        
+        # Valida se o nome da categoria está cadastrado
+        dicionario_categoria = None
+        for categoria in categorias:
+            if categoria["nome"] == nome:
+                dicionario_categoria = categoria
+                break
+
+        if not dicionario_categoria:
+            return False, f"\n⚠️ A categoria {nome} não está cadastrada."
+        
+        # Verifica se a categoria está em uso com algum produto, se estiver, não pode ser excluída
+        #TODO Criar a tabela e as funções de produtos para conseguir implementar essa regra de negócio
+        
+        # Remove o dicionário da categoria da base
+        categorias.remove(dicionario_categoria)
+
+        # Salva a lista atualizada na base
+        CategoriaDao.salvar_categoria(categorias)
+        return True, f"✅ A categoria {nome} foi deletada com sucesso."
+        
