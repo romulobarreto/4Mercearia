cbd5da454ecf389885ae3a53a2afa3d2b6b1f537
Romulo Barreto da Silva <romubah@gmail.com>
2025-04-18 13:26:37 -0300

    feat: adiciona a funcao de editar categoria

--- views/categoria_view.py
+++ views/categoria_view.py
@@ -30,3 +30,46 @@ class CategoriaView:
 
         # Exibe o resultado da função
         print(mensagem)
+
+
+
+    @staticmethod
+    def editar_categoria():
+        # Carrega a lista de categorias
+        categorias = CategoriaDao.carregar_categoria()
+
+        # Pega o input do usuário
+        nome = input("\nDigite a categoria que deseja editar: ").strip().lower()
+
+        # Valida se existe categoria cadastrada
+        if not categorias:
+            print("⚠️ Não existe nenhuma categoria para editar.")
+            return
+
+        # Valida o nome da categoria
+        if not nome:
+            print("⚠️ A categoria não pode estar vazia.")
+            return
+        
+        # Valida se o nome da categoria está cadastrado
+        dicionario_categoria = None
+        for categoria in categorias:
+            if categoria["nome"] == nome:
+                dicionario_categoria = categoria
+                break
+
+        if not dicionario_categoria:
+            print(f"\n⚠️ A categoria {nome} não está cadastrada.")
+            return
+        
+        # Mostra os detalhes da categoria selecionada
+        print(f"\nDetalhes da categoria:\nID: {dicionario_categoria["id"]}\nNome: {dicionario_categoria["nome"].upper()}")
+
+        # Input com o nome atualizado
+        novo_nome = input('\nDigite o novo nome da categoria (ou pressione Enter para manter o mesmo): ').strip().lower()
+
+        # Chama a função de editar a categoria
+        sucesso, mensagem = CategoriaController.editar_categoria(nome, novo_nome)
+
+        # Mostra a mensagem de sucesso
+        print(mensagem)
