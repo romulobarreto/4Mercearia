a729fa8043846d60da980a60d9a38bcbb0866092
Romulo Barreto da Silva <romubah@gmail.com>
2025-05-04 18:16:45 -0300

    feat: adiciona a tabela de cliente

---
+++ models/cliente.py
@@ -0,0 +1,9 @@
+from models.pessoa import Pessoa
+
+class Cliente(Pessoa):
+    def __init__(self, id, nome, cpf, telefone, endereco):
+        super().__init__(id, nome, cpf, telefone)
+        self.endereco = endereco
+
+    def salvar_dict(self):
+        return {"id": self.id, "nome": self.nome, "cpf": self.cpf, "telefone": self.telefone, "enedereco": self.endereco}
