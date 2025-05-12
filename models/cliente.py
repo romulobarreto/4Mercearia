6e8f97ab3c24dcab352e2a608a5ac1786c25bf8e
Romulo Barreto da Silva <romubah@gmail.com>
2025-05-11 21:03:34 -0300

    fix: ajusta o nome da key endereco ao transformar em dicionario

--- models/cliente.py
+++ models/cliente.py
@@ -6,4 +6,4 @@ class Cliente(Pessoa):
         self.endereco = endereco
 
     def salvar_dict(self):
-        return {"id": self.id, "nome": self.nome, "cpf": self.cpf, "telefone": self.telefone, "enedereco": self.endereco}
+        return {"id": self.id, "nome": self.nome, "cpf": self.cpf, "telefone": self.telefone, "endereco": self.endereco}
