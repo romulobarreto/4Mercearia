68794dc9ee314320f8e6c4d75f9e429a700f3ce6
ROMULO BARRETO DA SILVA <romubah@gmail.com>
2025-05-28 13:58:30 -0300

    chore: adiciona a funcao de cadastro de funcionario a view

---
+++ views/funcionario_view.py
@@ -0,0 +1,42 @@
+from controllers.funcionario_controller import *
+from views.cargo_view import *
+from utils.formatacao import formatar_cpf, formatar_telefone, formatar_preco
+from decimal import Decimal, InvalidOperation
+from utils.buscas import buscar_nome, buscar_nome
+
+class FuncionarioView():
+
+    @staticmethod
+    def cadastrar_funcionario():
+        # Carrega as listas de cargos e verifica se está vazia
+        cargos = CargoDao.carregar_cargos()
+        
+        if not cargos:
+            print("\n🚫 Não existe nenhum cargo cadastrado, não é possível cadastrar um funcionário.")
+            return
+        
+        # Solicita os inputs ao usuário
+        nome = input("\nDigite o nome do funcionário: ").strip().lower()
+
+        cpf = input("\nDigite o CPF do funcionário: ").strip().replace(".", "").replace("-", "")
+
+        telefone = input("\nDigite o telefone do funcionário (DDD + Número):  ").strip().replace("(", "").replace(")", "").replace("-","").replace(" ","")
+
+        # Exibe os cargos disponíveis para cadastro
+        CargoView.detalhar_cargos()
+
+        try:
+            cargo_id = int(input("\nDigite o ID do cargo que será atribuído ao funcionário: ").strip())
+        except ValueError:
+            print("\n⚠️ O valor não está na formatação correta.")
+            return
+        
+        try:
+            salario = Decimal(input("\nDigite o salário do funcionário: R$").strip().replace(".", "").replace(",", "."))
+        except InvalidOperation:
+            print("\n⚠️ O valor não está na formatação correta.")
+            return
+        
+        # Chama a função de cadastrar funcionário
+        sucesso, mensagem = FuncionarioController.cadastrar_funcionario(nome, cpf, telefone, cargo_id, salario)
+        print(mensagem)
