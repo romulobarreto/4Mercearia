137e7bf5917fd2b4c2cec1ac70441f47819f0a93
Romulo Barreto da Silva <romubah@gmail.com>
2025-05-27 23:58:18 -0300

    refactor: ajusta o id do cargo para seguir o mesmo padrao de produto

--- models/funcionario.py
+++ models/funcionario.py
@@ -1,10 +1,10 @@
 from models.pessoa import Pessoa
 
 class Funcionario(Pessoa):
-    def __init__(self, id, nome, cpf, telefone, id_cargo, salario):
+    def __init__(self, id, nome, cpf, telefone, cargo_id, salario):
         super().__init__(id, nome, cpf, telefone)
-        self.id_cargo = id_cargo
+        self.cargo_id = cargo_id
         self.salario = salario
 
     def salvar_dict(self):
-        return {"id": self.id, "nome": self.nome, "cpf": self.cpf, "telefone": self.telefone, "cargo": self.id_cargo, "salario": self.salario}
+        return {"id": self.id, "nome": self.nome, "cpf": self.cpf, "telefone": self.telefone, "cargo": self.cargo_id, "salario": self.salario}
