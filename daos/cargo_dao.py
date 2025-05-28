c1323ea68fece9d5b77e79a2dd29d9a9fac75820
Romulo Barreto da Silva <romubah@gmail.com>
2025-05-27 23:36:09 -0300

    chore: adiciona a DAO de cargo

---
+++ daos/cargo_dao.py
@@ -0,0 +1,25 @@
+import json
+import os
+
+class CargoDao:
+    # Define o caminha padrão do banco de categoria
+    caminho_arquivo = os.path.join("database", "cargo.json")
+    
+    # Carrega a lista de categorias, caso não haja lista, traz uma lista vazia
+    @classmethod
+    def carregar_cargo(cls):
+        try:
+            with open(cls.caminho_arquivo, "r") as arq:
+                return json.load(arq)
+        except (FileNotFoundError, json.JSONDecodeError):
+            return []
+        
+    # Salva as alterações no arquivo, reescrevendo a base e retornando mensagem    
+    @classmethod    
+    def salvar_categoria(cls, cargos):
+        try:
+            with open(cls.caminho_arquivo, "w") as arq:
+                json.dump(cargos, arq, indent=4)
+                return True, "\n✅ Dados salvos com sucesso."
+        except Exception as e:
+            return False, "❌ Dados não salvos por erro, favor, validar."
