7515627ca97483a53026536646dec6e0cac078c3
Romulo Barreto da Silva <romubah@gmail.com>
2025-05-27 23:33:25 -0300

    fix: ajusta a funcao de salvar dict para salvar o cargo correto

--- models/funcionario.py
+++ models/funcionario.py
@@ -1,10 +1,10 @@
 from models.pessoa import Pessoa
 
 class Funcionario(Pessoa):
-    def __init__(self, id, nome, cpf, telefone, cargo, salario):
+    def __init__(self, id, nome, cpf, telefone, id_cargo, salario):
         super().__init__(id, nome, cpf, telefone)
-        self.cargo = cargo
+        self.id_cargo = id_cargo
         self.salario = salario
 
     def salvar_dict(self):
-        return {"id": self.id, "nome": self.nome, "cpf": self.cpf, "telefone": self.telefone, "cargo": self.cliente, "salario": self.salario}
+        return {"id": self.id, "nome": self.nome, "cpf": self.cpf, "telefone": self.telefone, "cargo": self.id_cargo, "salario": self.salario}
