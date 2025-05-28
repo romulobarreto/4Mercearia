d0f6f657f0529ba0ea9656ee2f289daa25a12a72
Romulo Barreto da Silva <romubah@gmail.com>
2025-05-27 23:55:40 -0300

    refactor: ajusta as funcoes de buscas para aceitar parametro de qualquer tabela do sistema e trazer o resultado da funcao

--- utils/buscas.py
+++ utils/buscas.py
@@ -1,34 +1,15 @@
-# Buscas de categoria
-def criar_dict_categorias(categorias):
-    return {categoria["id"]: categoria["nome"] for categoria in categorias}
+# Funções de busca
+def criar_dict(lista):
+    # Cria lista simplificada de ID na chave com Nome 
+    return {item["id"]: item["nome"] for item in lista}
 
 
-def buscar_id_categoria(categoria_nome, categorias):
-    # Busca o ID da categoria a partir do nome
-    categoria_id = next((categoria["id"] for categoria in categorias if categoria["nome"] == categoria_nome), None)
-    return categoria_id
+def buscar_id(nome, lista):
+    # Busca o ID a partir do nome
+    item_id = next((item["id"] for item in lista if item["nome"] == nome), None)
+    return item_id
 
-def buscar_nome_categoria(categoria_id, categorias):
-    # Busca o nome da categoria a partir do ID
-    categoria_nome = next((categoria["nome"] for categoria in categorias if categoria["id"] == categoria_id))
-    return categoria_nome
-
-
-
-
-
-# Buscas de fornecedor
-def criar_dict_fornecedores(fornecedores):
-    return {fornecedor["id"]: fornecedor["nome"] for fornecedor in fornecedores}
-
-
-def buscar_id_fornecedor(fornecedor_nome, fornecedores):
-    # Busca o ID do fornecedor a partir do nome
-    fornecedor_id = next((fornecedor["id"] for fornecedor in fornecedores if fornecedor["nome"] == fornecedor_nome), None)
-    return fornecedor_id
-
-
-def buscar_nome_fornecedor(fornecedor_id, fornecedores):
-    # Busca o nome do fornecedor a partir do ID
-    fornecedor_nome = next((fornecedor["nome"] for fornecedor in fornecedores if fornecedor["id"] == fornecedor_id))
-    return fornecedor_nome
+def buscar_nome(id, lista):
+    # Busca o nome a partir do ID
+    item_nome = next((item["nome"] for item in lista if item["id"] == id))
+    return item_nome
