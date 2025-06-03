2333097308a65570c5b9966007c18a0d29078904
ROMULO BARRETO DA SILVA <romubah@gmail.com>
2025-06-03 13:56:31 -0300

    feat: adiciona a tabela de vendas

---
+++ models/venda.py
@@ -0,0 +1,12 @@
+class Venda():
+    def __init__(self, id, funcionario_id, data, itens, total, cliente_id=None):
+        self.id = id
+        self.funcionario_id = funcionario_id
+        self.cliente_id = cliente_id
+        self.data = data
+        self.itens = itens
+        self.total = total
+
+    # Salvar em forma de dicionário
+    def salvar_dict(self):
+        return {"id": self.id, "funcionario_id": self.funcionario_id, "cliente_id": self.cliente_id, "data": self.data, "itens": self.itens, "total": str(self.total)}
