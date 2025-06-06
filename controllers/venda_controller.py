076aceca956ae9c63f8fd6fce32f4e1dafe25d01
ROMULO BARRETO DA SILVA <romubah@gmail.com>
2025-06-06 14:00:58 -0300

    chore: adiciona a funcao de cadastrar venda ao controller

--- controllers/venda_controller.py
+++ controllers/venda_controller.py
@@ -1,8 +1,11 @@
 from views.produto_view import *
+from daos.venda_dao import *
 from datetime import date
 from utils.formatacao import formatar_preco
 from utils.buscas import criar_dict
 from decimal import Decimal
+from utils.gerador import gerador_id
+from models.venda import *
 
 
 class VendaController():
@@ -34,3 +37,44 @@ class VendaController():
                 )
         
         return True, lista_formatada
+    
+
+
+
+
+
+
+    @staticmethod
+    def cadastrar_venda(funcionario_id, cliente_id, itens):
+        # Carrega a lista de vendas e produtos
+        vendas = VendaDao.carregar_vendas()
+        produtos = ProdutoDao.carregar_produto()
+
+        # Gera o ID da venda 
+        id = gerador_id(vendas)
+
+        # Pega a data da venda
+        data = date.today()
+
+        # Gera o total da venda
+        lista_precos = []
+        for item in itens:
+            lista_precos.append(item["quantidade"] * Decimal(item["preco"]))
+        total = sum(lista_precos)
+
+        # Cria a venda
+        venda = Venda(id, funcionario_id, data, itens, total, cliente_id)
+
+        # Adiciona a venda na lista de vendas e salva em JSON
+        vendas.append(venda.salvar_dict(venda))
+        sucesso, mensagem = VendaDao.salvar_vendas(vendas)
+
+        if sucesso:
+            for item in itens:
+                for produto in produtos:
+                    if item["produto_id"] == produto["id"]:
+                        produto["quantidade"] -= item["quantidade"]
+            ProdutoDao.salvar_produto(produtos)
+            return True, mensagem
+        else:
+            return False, mensagem
