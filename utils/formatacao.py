9be881a5ac84d735390a7b792f891678d81da4d6
Romulo Barreto da Silva <romubah@gmail.com>
2025-06-08 19:07:00 -0300

    chore: adiciona a funcao de formatar data e de formatar preco para banco de dados

--- utils/formatacao.py
+++ utils/formatacao.py
@@ -1,3 +1,4 @@
+from datetime import date
 def formatar_telefone(telefone):
         # Formata o número do telefone de acordo com o padrão brasileiro
         ddd = telefone[0:2]
@@ -27,3 +28,24 @@ def formatar_preco(preco):
      # Formata o preço para ser exibido no formato Real(R$)
      preco_formatado = f"R${preco:,.2f}".replace(",", "v").replace(".",",").replace("v",".")
      return preco_formatado
+
+
+
+
+
+
+def formatar_preco_dados(preco):
+     # Formata o preço para ser exibido no formato Real(R$)
+     preco_formatado = f"{preco:,.2f}".replace(",", "v").replace(".",",").replace("v",".")
+     return preco_formatado
+
+
+
+
+
+
+def formatar_data_br(data):
+    # Garante que 'data' seja um objeto datetime. Se já for, ignora.
+    if isinstance(data, str):
+        data = date.fromisoformat(data)
+    return data.strftime("%d/%m/%Y")
